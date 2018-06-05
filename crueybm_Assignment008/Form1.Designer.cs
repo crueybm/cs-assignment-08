454f98632df119d87379ed0ed9e51375d7d50e37
crueybm <crueybm@mail.uc.edu>
2018-06-05 04:26:20 -0400

    Add project files.

---
+++ crueybm_Assignment008/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace crueybm_Assignment008
+{
+    partial class frmZeller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.tbYear = new System.Windows.Forms.TextBox();
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.cbDate = new System.Windows.Forms.ComboBox();
+            this.lblChoose = new System.Windows.Forms.Label();
+            this.lbCelebrity = new System.Windows.Forms.ListBox();
+            this.pbCelebrity = new System.Windows.Forms.PictureBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCelebrity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonth.Location = new System.Drawing.Point(12, 9);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(53, 16);
+            this.lblMonth.TabIndex = 0;
+            this.lblMonth.Text = "Month:";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.Location = new System.Drawing.Point(12, 34);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(40, 16);
+            this.lblDay.TabIndex = 1;
+            this.lblDay.Text = "Day:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Year:";
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRes.Location = new System.Drawing.Point(12, 103);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(145, 16);
+            this.lblRes.TabIndex = 3;
+            this.lblRes.Text = "You were born on a:";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(15, 122);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(142, 20);
+            this.tbResult.TabIndex = 4;
+            // 
+            // tbYear
+            // 
+            this.tbYear.Location = new System.Drawing.Point(90, 61);
+            this.tbYear.Name = "tbYear";
+            this.tbYear.Size = new System.Drawing.Size(67, 20);
+            this.tbYear.TabIndex = 5;
+            this.tbYear.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cbMonth.Location = new System.Drawing.Point(62, 4);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(95, 21);
+            this.cbMonth.TabIndex = 6;
+            this.cbMonth.SelectedIndexChanged += new System.EventHandler(this.cbMonth_SelectedIndexChanged);
+            // 
+            // cbDate
+            // 
+            this.cbDate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDate.FormattingEnabled = true;
+            this.cbDate.Location = new System.Drawing.Point(99, 31);
+            this.cbDate.Name = "cbDate";
+            this.cbDate.Size = new System.Drawing.Size(58, 21);
+            this.cbDate.TabIndex = 7;
+            this.cbDate.SelectedIndexChanged += new System.EventHandler(this.cbDate_SelectedIndexChanged);
+            // 
+            // lblChoose
+            // 
+            this.lblChoose.AutoSize = true;
+            this.lblChoose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChoose.Location = new System.Drawing.Point(163, 9);
+            this.lblChoose.Name = "lblChoose";
+            this.lblChoose.Size = new System.Drawing.Size(135, 16);
+            this.lblChoose.TabIndex = 9;
+            this.lblChoose.Text = "Select a Celebrity:";
+            // 
+            // lbCelebrity
+            // 
+            this.lbCelebrity.FormattingEnabled = true;
+            this.lbCelebrity.Location = new System.Drawing.Point(163, 28);
+            this.lbCelebrity.Name = "lbCelebrity";
+            this.lbCelebrity.Size = new System.Drawing.Size(142, 69);
+            this.lbCelebrity.TabIndex = 10;
+            this.lbCelebrity.SelectedIndexChanged += new System.EventHandler(this.lbCelebrity_SelectedIndexChanged);
+            // 
+            // pbCelebrity
+            // 
+            this.pbCelebrity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCelebrity.Location = new System.Drawing.Point(163, 103);
+            this.pbCelebrity.Name = "pbCelebrity";
+            this.pbCelebrity.Size = new System.Drawing.Size(142, 134);
+            this.pbCelebrity.TabIndex = 11;
+            this.pbCelebrity.TabStop = false;
+            this.pbCelebrity.SizeModeChanged += new System.EventHandler(this.pbCelebrity_SizeModeChanged);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(15, 214);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(50, 23);
+            this.btnOK.TabIndex = 12;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(82, 214);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmZeller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 243);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.pbCelebrity);
+            this.Controls.Add(this.lbCelebrity);
+            this.Controls.Add(this.lblChoose);
+            this.Controls.Add(this.cbDate);
+            this.Controls.Add(this.cbMonth);
+            this.Controls.Add(this.tbYear);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblMonth);
+            this.Name = "frmZeller";
+            this.Text = "Zeller\'s Algorithm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbCelebrity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.TextBox tbYear;
+        private System.Windows.Forms.ComboBox cbMonth;
+        private System.Windows.Forms.ComboBox cbDate;
+        private System.Windows.Forms.Label lblChoose;
+        private System.Windows.Forms.ListBox lbCelebrity;
+        private System.Windows.Forms.PictureBox pbCelebrity;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ crueybm_Assignment008/Form1.cs
@@ -0,0 +1,254 @@
+﻿/*
+ * Name: Brandon Cruey 
+ * email: crueybm@mail.uc.edu
+ * Class: Contemporary Programming
+ * Date: 10/19/2017
+ * Assignment: 008
+ * Description: This program collects three input fields from a user, and then runs the gathered variables through
+ * Zeller's Algorithm to determine the day of the week that they were born.  The date is then populated to a TextBox.
+ * At this point, the user can reset the inputs to start over.  A list of celebrities is also provided for some examples,
+ * and clicking on a celebrity's name will populate the inputs as well as display a picture of said celebrity.
+ * Citation: https://stackoverflow.com/questions/24764353/load-picturebox-image-in-c-sharp-from-file-in-relative-path
+ * Citation: https://stackoverflow.com/questions/12646287/image-scaling-of-picture-box
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace crueybm_Assignment008
+{
+
+    public partial class frmZeller : Form
+    {
+        int a;                                  //declares int a
+        int b;                                  //declares int b
+        ArrayList celeb = new ArrayList();      //declares and instantiates ArrayList "celeb"
+        string cel = "";                        //Declares string cel and sets it to an empty string
+
+        public frmZeller()
+        {
+            InitializeComponent();
+            this.pbCelebrity.SizeMode = PictureBoxSizeMode.Zoom;    //fits images to picturebox
+            celeb.Add("Calbert Cheaney");       //Adds Calbert Cheaney to ArrayList
+            celeb.Add("Grace Hopper");          //Adds Grace Hopper to ArrayList
+            celeb.Add("Douglas Adams");         //Adds Douglas Adams to ArrayList
+            celeb.Add("John Wayne");            //Adds John Wayne to ArrayList
+            celeb.Add("Artemis");               //Adds Artemis to ArrayList
+            for (int i = 0; i < celeb.Count; i++)   //Starts a for-loop that adds the array items to lbCelebrity
+            {
+                lbCelebrity.Items.Add(celeb[i]);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar)) //Limits tbYear to only take numerical input
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void cbMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbDate.Items.Clear();                           //Clears existing items from cbDate
+            string mon = cbMonth.SelectedItem.ToString();   //Creates a string from the selected month
+            int len = 0;                                    //Declares int that determines the length of the month
+            
+            switch (mon)                                    //Switch statement to set variable data for int b and int len
+            {
+                case "January":                             //Case January sets int b to 13 per algorithm
+                    b = 13;
+                    len = 31;
+                    break;
+                case "February":                            //Case February sets int b to 14 per algorithm
+                    b = 14;
+                    len = 29;
+                    break;
+                case "March":
+                    b = 3;
+                    len = 31;
+                    break;
+                case "April":
+                    b = 4;
+                    len = 30;
+                    break;
+                case "May":
+                    b = 5;
+                    len = 31;
+                    break;
+                case "June":
+                    b = 6;
+                    len = 30;
+                    break;
+                case "July":
+                    b = 7;
+                    len = 31;
+                    break;
+                case "August":
+                    b = 8;
+                    len = 31;
+                    break;
+                case "September":
+                    b = 9;
+                    len = 30;
+                    break;
+                case "October":
+                    b = 10;
+                    len = 31;
+                    break;
+                case "November":
+                    b = 11;
+                    len = 30;
+                    break;
+                case "December":
+                    b = 12;
+                    len = 31;
+                    break;
+            }
+
+            for (int i = 1; i < len + 1; i++)   //For-loop that sets the contents of cbDate to var len
+            {
+                cbDate.Items.Add(i);
+            }
+
+        }
+
+        private void cbDate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(cbDate.SelectedItem);   //sets int a to the selected item from cbDate
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            tbResult.Clear();                       //Clears text from tbResult
+            int c = Convert.ToInt32(tbYear.Text);   //Accepts input from tbYear and converts it to an int
+            string final = Zeller(a, b, c);         //Creates string final and calls upon method Zeller for the result
+            tbResult.Text = final;                  //Sets tbResult to the returned string from Zeller
+        }
+
+        private string Zeller(int q, int m, int y)  //Declares method Zeller, which accepts three integers as arguments
+        {
+            string warning = "This is not a leap year.";    //Creates a warning for when a user selects February 29th on a non leap year.
+            string date = "";                       //Creates an empty string for later use
+            int h;                                  //Declares int h for later use
+            if (m == 14 && q == 29)                 //An if-statement to catch invalid leap year entry
+            {
+                if (y % 4 != 0)                     //If year mod 4 doesn't equal 0, return the warning string
+                {
+                    return warning;
+                }
+            }
+            if (m > 12)                             //If month is greater than 12 (Jan = 13 or Feb = 14 per algorithm)
+            {
+                y--;                                //Adjust year to year -1
+            } 
+            int k = (y % 100);                      //Declares int k to equal year mod 100
+            int j = (y / 100);                      //Declares int j to equal year / 100
+            int total = q;                          //Declares int total to equal q
+            total += (13 * (m + 1)) / 5;            //Adds given equation to int total
+            total += (k + (k / 4));                 //Adds given equation to int total
+            total += (j / 4) + (5 * j);             //Adds given equation to int total
+            h = (total % 7);                        //Sets int h to total mod 7
+            switch (h)                              //Starts a switch statement for int h, to set string date to a day of the week
+            {
+                case 0:
+                    date = "Saturday";
+                    break;
+                case 1:
+                    date = "Sunday";
+                    break;
+                case 2:
+                    date = "Monday";
+                    break;
+                case 3:
+                    date = "Tuesday";
+                    break;
+                case 4:
+                    date = "Wednesday";
+                    break;
+                case 5:
+                    date = "Thursday";
+                    break;
+                case 6:
+                    date = "Friday";
+                    break;
+
+            }
+            return date;                            //Returns string date for the method
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e) //Handles button click for reset
+        {
+            cbMonth.SelectedIndex = 0;              //Sets cbMonth to index 0
+            cbDate.SelectedIndex = 0;               //Sets cbDate to index 0
+            tbYear.Clear();                         //Clears tbYear
+            tbResult.Clear();                       //Clears tbResult
+            lbCelebrity.ClearSelected();            //Clears selected item from lbCelebrity
+            pbCelebrity.Image = null;               //Sets pbCelebrity to null/no picture
+        }
+
+        private void lbCelebrity_SelectedIndexChanged(object sender, EventArgs e)   //Handles an index being selected in lbCelebrity
+        {
+            if (lbCelebrity.Text == "Calbert Cheaney")  //If statement for Calbert Cheaney to set the month, date, year, and picturebox
+            {
+                cbMonth.SelectedItem = "July";
+                cbDate.SelectedItem = 17;
+                tbYear.Text = "1971";
+                pbCelebrity.ImageLocation = @"..\..\images\ccheaney.jpg";
+                int c = Convert.ToInt32(tbYear.Text);
+                cel = Zeller(a, b, c);
+                tbResult.Text = cel;
+            } else if (lbCelebrity.Text == "Grace Hopper")  //Else-if statement for Grace Hopper to set the month, date, year, and picturebox
+            {
+                cbMonth.SelectedItem = "December";
+                cbDate.SelectedItem = 9;
+                tbYear.Text = "1906";
+                pbCelebrity.ImageLocation = @"..\..\images\ghopper.jpg";
+                int c = Convert.ToInt32(tbYear.Text);
+                cel = Zeller(a, b, c);
+                tbResult.Text = cel;
+            } else if (lbCelebrity.Text == "Douglas Adams") //Else-if statement for Douglas Adams to set the month, date, year, and picturebox
+            {
+                cbMonth.SelectedItem = "March";
+                cbDate.SelectedItem = 11;
+                tbYear.Text = "1952";
+                pbCelebrity.ImageLocation = @"..\..\images\dadams.jpg";
+                int c = Convert.ToInt32(tbYear.Text);
+                cel = Zeller(a, b, c);
+                tbResult.Text = cel;
+            } else if (lbCelebrity.Text == "John Wayne")    //Else-if statement for John Wayne to set the month, date, year, and picturebox
+            {
+                cbMonth.SelectedItem = "May";
+                cbDate.SelectedItem = 26;
+                tbYear.Text = "1907";
+                pbCelebrity.ImageLocation = @"..\..\images\jwayne.jpg";
+                int c = Convert.ToInt32(tbYear.Text);
+                cel = Zeller(a, b, c);
+                tbResult.Text = cel;
+            } else if (lbCelebrity.Text == "Artemis")       //Else-if statement for Artemis to set the month, date, year, and picturebox
+            {
+                cbMonth.SelectedItem = "July";
+                cbDate.SelectedItem = 4;
+                tbYear.Text = "2012";
+                pbCelebrity.ImageLocation = @"..\..\images\artemis.jpg";
+                int c = Convert.ToInt32(tbYear.Text);
+                cel = Zeller(a, b, c);
+                tbResult.Text = cel;
+            }
+        }
+
+        private void pbCelebrity_SizeModeChanged(object sender, EventArgs e)
+        {
+            this.pbCelebrity.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+    }
+}
